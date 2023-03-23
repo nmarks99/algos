1b69b0dfa03388bef9b794393e0793d7df591c0f
Nick Marks <nmarks99@gmail.com>
2023-03-23 18:47:06 -0500

    Save

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -8,10 +8,10 @@ fn main() {
 // ------------------------------------------------------------
 
     // println!("Quick sort:");
-    // let mut arr_copy = arr.clone();
+    // let mut arr = arr.clone();
 //
     // let start = Instant::now();
-    // quicksort(&mut arr_copy);
+    // quicksort(&mut arr);
     // let duration = start.elapsed();
     //
     // println!("Duration = {} ns",duration.as_nanos() as f64);
@@ -23,16 +23,12 @@ fn main() {
     println!("\nBubble sort:");
     let mut rng = rand::thread_rng();
     let range = Uniform::new(-100,100);
-    let arr: Vec<i64> = (-50..50).map(|_| rng.sample(&range)).collect();
+    let mut arr: Vec<i64> = (-50..50).map(|_| rng.sample(&range)).collect();
     
-    println!("len = {}", arr.len());
-    println!("arr = {:?}", arr);
-    let mut arr_copy = arr.clone();
-
     let mut times: Vec<f64> = Vec::new();
     for _ in 0..ITERATIONS{
         let start = Instant::now();
-        bubblesort(&mut arr_copy);
+        bubblesort(&mut arr);
         let duration = start.elapsed();
         times.push(duration.as_micros() as f64);
     }
