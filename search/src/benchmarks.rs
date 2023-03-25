9d37eabe3bcfce21e7ba4f06bdb59879d5c92d7c
Nick Marks <nmarks99@gmail.com>
2023-03-24 22:50:59 -0500

    I think binary search is insanely fast?

--- search/src/benchmarks.rs
+++ search/src/benchmarks.rs
@@ -1,6 +1,6 @@
 use search::binary_search;
 use rand::{distributions::Uniform, Rng};
-// use std::time::Instant;
+use std::time::Instant;
 
 
 // Fills a vector with n random values between min_rand and max_rand
@@ -12,32 +12,33 @@ fn gen_random_vec(n: usize, min_rand: i64, max_rand: i64) -> Vec<i64> {
     arr
 }
 
+// computes the average value of a Vec<f64>
+// fn vec_avg(vec: &Vec<f64>) -> f64 {
+    // let mut avg: f64 = 0.0;
+    // for i in 0..vec.len() {
+        // avg += vec[i];
+    // }
+    // avg / vec.len() as f64
+// }
+
 fn main() { 
     
+    const MIN_RAND: i64 = -1000;
+    const MAX_RAND: i64 = 1000;
+    const ARR_SIZE: usize = 100000;
+    
     // Get a random vector and sort it
-    const MIN_RAND: i64 = -100;
-    const MAX_RAND: i64 = 100;
-    const ARR_SIZE: usize = 10;
-
     let mut v = gen_random_vec(ARR_SIZE, MIN_RAND, MAX_RAND);
     v.sort();
-    println!("{:?}",v);
-    println!("{:?}",v.len());
 
     // Get a random target value
     let mut rng = rand::thread_rng();
-    let rand_ind = rng.gen_range(0..ARR_SIZE);
-    println!("rand_ind = {}", rand_ind);
-    let target_value = v[rand_ind];
+    let target_value = v[rng.gen_range(0..ARR_SIZE)];
 
-    let ind = binary_search(&v, target_value);
+    // Run binary search and time it
+    let start = Instant::now();
+    let _ind = binary_search(&v, target_value);
+    let elapsed = start.elapsed();
 
-    if ind.is_none() {
-        println!("Target value {} not found in vector", target_value);
-    }
-    else 
-    {
-        println!("target value {} at index {}", target_value, ind.unwrap());
-    }
-
+    println!("Elapsed time = {}", elapsed.as_nanos());
 }
